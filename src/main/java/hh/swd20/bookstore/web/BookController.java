cfe4b8476a77eee86062bb5e644681ccfeba4ae7
Lum <bgu195@myy.haaga-helia.fi>
2022-09-12 16:54:46 +0300

    initial commit

---
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -0,0 +1,15 @@
+package hh.swd20.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+	
+	@GetMapping("/index")
+	public String getBooks(Model model) {
+		return "booklist"; // booklist.html
+	}
+
+}
