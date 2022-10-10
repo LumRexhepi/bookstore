5253c5e8122d1db7bda5542e401874c99239155f
Lum <bgu195@myy.haaga-helia.fi>
2022-10-10 21:43:03 +0300

    Added user entity and db

--- src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -1,6 +1,7 @@
 package hh.swd20.bookstore;
 
 
+import org.apache.commons.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +13,8 @@ import hh.swd20.bookstore.domain.Book;
 import hh.swd20.bookstore.domain.BookRepository;
 import hh.swd20.bookstore.domain.Category;
 import hh.swd20.bookstore.domain.CategoryRepository;
+import hh.swd20.bookstore.domain.User;
+import hh.swd20.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -23,7 +26,7 @@ public class BookstoreApplication {
 }
 
 	@Bean
-	public CommandLineRunner demo(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner demo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of Books");
 			Category c_a = new Category("Fiction");
@@ -45,6 +48,11 @@ public class BookstoreApplication {
 			repository.save(c);
 			repository.save(d);
 			
+
+			urepository.save(new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER"));
+			urepository.save(new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN"));
+			
+			
 			log.info("Fetch all the categories");
 			for (Category category: crepository.findAll()) {
 				log.info(category.toString());
@@ -55,6 +63,8 @@ public class BookstoreApplication {
 			for (Book book : repository.findAll()) {
 				log.info(book.toString());
 			}
+			
+			
 		};
 	}
 
--- src/main/java/hh/swd20/bookstore/WebSecurityConfig.java
+++ src/main/java/hh/swd20/bookstore/WebSecurityConfig.java
@@ -2,33 +2,44 @@ package hh.swd20.bookstore;
 
 
 
-import java.util.ArrayList;
-import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
-
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+	@Autowired
+	private UserDetailsService UserDetailsService;
+
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/", "/home").permitAll()
+			 .antMatchers("/h2-console/**").permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
+			  .csrf().ignoringAntMatchers("/h2-console/**")
+			  .and()
+			  .headers().frameOptions().sameOrigin()
+			  .and()
 		.formLogin()		
 			.loginPage("/login")
-			.defaultSuccessUrl("/booklist")
+			.defaultSuccessUrl("/booklist", true)
 			.permitAll()
 			.and()
 		.logout()
@@ -38,27 +49,11 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 	
-	@Bean 
-	public InMemoryUserDetailsManager userDetailsService() {
-		
-		List<UserDetails> users = new ArrayList();
-	        UserDetails user1 = User.withDefaultPasswordEncoder()
-	            .username("user")
-	            .password("password")
-	            .roles("USER")
-	            .build();
-	       users.add(user1);
-	        
-	        UserDetails user2 = User.withDefaultPasswordEncoder()
-		            .username("admin")
-		            .password("password")
-		            .roles("ADMIN")
-		            .build();
-	        users.add(user2);
-	        
-		   return new InMemoryUserDetailsManager(users);
-  }
-
-
-
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(UserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 }
+
+
+
