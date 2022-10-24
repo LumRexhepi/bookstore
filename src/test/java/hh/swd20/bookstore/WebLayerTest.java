075881261e2cdb7428d1427248bf92068a12ffd3
Lum <bgu195@myy.haaga-helia.fi>
2022-10-24 13:39:21 +0300

    Sovellus kaatunut en tiedä miksi

--- src/test/java/hh/swd20/bookstore/WebLayerTest.java
+++ src/test/java/hh/swd20/bookstore/WebLayerTest.java
@@ -1,31 +1,33 @@
 package hh.swd20.bookstore;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static org.hamcrest.Matchers.containsString;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class) // JUnit5 eli Jupiter
 @SpringBootTest
 @AutoConfigureMockMvc
+
 public class WebLayerTest {
 
-	@Autowired
-	private MockMvc mockMvc;
+	//@Autowired
+	//private MockMvc mockMvc;
 
 	/*@Test
 	public void testDefaultMessage() throws Exception {
 		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().is())
-				.andExpect(view().name("redirect:/events"));
+				.andExpect(view().name("redirect:/booklist"));
 	}*/
 
 }
